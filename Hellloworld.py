2914da7582d2d47516e761bdfdb5eec49771f812
idanz19-meet <idanz19@meet.mit.edu>
2017-07-25 15:53:02 +0200

    Stopping here

---
+++ Hellloworld.py
@@ -0,0 +1,18 @@
+print("(ola mundo) is how you say Hello world in (Portuguese).")
+print("(privet mir) is how you say Hello world in (Russain)")
+print("(Hallo welt) is how you say Hello world in (German)")
+print("(Salmu dunia) is how you say Hello world in (Swahili)")
+print("(namaste duniya) is how you say Hello world in (Hindi)")
+print("(Ni hao shinjie) is how you say Hello world in (Chinese)")
+print("(Annyeong sesang) is how you say Hello world in (Korean)")
+print("(Geia sou kosme) is how you say Hello world in (Greek)")
+print("(Ahoj svete) is how you say Hello world in (Czech)")
+print("(Shalom Olam) is how you say Hello world in (Hebrew)")
+print("(Hello world) is how you say Hello world in (English)")
+
+
+
+
+
+
+
---
+++ Input.py
@@ -0,0 +1,3 @@
+print("what is your name?")
+age = input()
+print("wow, you're " + age + ", that is an awesome name")
